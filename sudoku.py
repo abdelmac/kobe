0f3f73a7748aa6f96817cbb9e4d4bedd8c7bfbdb
abdelmac <abdelkadermachta@gmail.com>
2024-10-08 17:48:57 +0200

    ds

---
+++ sudoku.py
@@ -0,0 +1,14 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+for i in range(9):
+    for j in input().split():
+        n = int(j)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("true or false")
