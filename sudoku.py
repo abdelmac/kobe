acf86da958e5f56a7c57f6ffca4f13aa8fcd6870
abdelmac <abdelkadermachta@gmail.com>
2024-10-08 18:54:54 +0200

    re

--- sudoku.py
+++ sudoku.py
@@ -10,15 +10,17 @@ for i in range(9):
     for j in input().split():
         
         k=j.split()
-        
+
         n = int(j)
-        L.append(k)
+        L.append(n)
 
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
 print("true or false")
+
+print(n)
 
 print(L[1])
 
