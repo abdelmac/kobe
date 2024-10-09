557dfa9548c28439d52b31df7784ec67bde4a67e
abdelmac <abdelkadermachta@gmail.com>
2024-10-09 16:49:52 +0200

    dz

--- sudo.py
+++ sudo.py
@@ -12,13 +12,9 @@ import math
 
 
 L=[]
-A=[]
+L=[[1,2,3,4,5,6,7,8,9],[9,6,4,5,2,3,1,7,8],[9,6,4,5,2,3,1,7,8],[9,6,4,5,2,3,1,7,8],[9,6,4,5,2,3,1,7,8],[9,6,4,5,2,3,1,7,8],[9,6,4,5,2,3,1,7,8],[9,6,4,5,2,3,1,7,8],[9,8,7,6,5,4,3,2,1]]
 M=[]
 
-for i in range(9):
-    A.append(0)
-for i in range(9):
-    L.append(A)
 
 for i in range(9):
     for j in input().split():        
@@ -30,9 +26,9 @@ for i in range(9):
     L[i]=M
     M=[]
 
-
-for i in range(9):
-    print(L[i])
+for i in range(3):
+    for j in range(3):
+        print(L[i+1][j])
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
