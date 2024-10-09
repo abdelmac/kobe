97986196f39cbaf0ad60e532be93318a228f8f88
abdelmac <abdelkadermachta@gmail.com>
2024-10-09 09:42:58 +0200

    xq

--- sudoku.py
+++ sudoku.py
@@ -18,38 +18,25 @@ L=[]
 
 
 for i in range(9):
-    for j in input().split():
-        
-        k=j.split()
-
+    for j in input().split():        
         n = int(j)
         print(n)
 
-
-        L[i].append(n)
-    
-    print(L)
 
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-print("true or false")
-
-print(n)
-
-print(L[1])
-
 reponse="true"
 
 for i in range(9):
     for j in range(9):
-        if L[i][i]==L[i][j]:
+        if L[i]==L[i]:
             reponse="false"
 
 for i in range(9):
     for j in range(9):
-        if L[i][i]==L[j][i]:
+        if L[i]==L[j]:
             reponse="false"
 
 
