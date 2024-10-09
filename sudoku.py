af2cae27f31103ca6f41cab0077c0cb83e46469b
abdelmac <abdelkadermachta@gmail.com>
2024-10-09 10:25:38 +0200

    cs

--- sudoku.py
+++ sudoku.py
@@ -4,30 +4,35 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-M=[]
+L=[]
 A=[]
 
 for i in range(9):
     A.append(0)
 for i in range(9):
-    M.append(A)
-for i in range(9):    
-    print(M[i])
-
+    L.append(A)
 
 for i in range(9):
     for j in input().split():        
         n = int(j)
         a=int(i)-1
         b=int(j)-1
-        M[a][b]=n
+        L[a][b]=n
 
-for i in range(9):
-    print(M[i])
+#for i in range(9):
+#    print(L[i])
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
 reponse="true"
 
+for i in range(9):
+    for j in range(9):
+        if L[i][i]==L[i][j]:
+            reponse="false"
 
+
+
+
+print(reponse)
