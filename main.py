4dee1a77d65b08dff56cb22e9c19daff1c9704bc
abdelmac <abdelkadermachta@gmail.com>
2024-10-29 14:57:04 +0100

    4

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ phi0 = 2 * np.pi / 3  # Phase initiale
 
 fc = f0 * 100  # Échantillonnage pour le signal continu
 
-start = -20e-3  # Temps de départ12345
+start = -20e-3  # Temps de départ1234
 stop = 20e-3    # Temps d'arrêt
 
 # Signal échantillonné
