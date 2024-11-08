e1019dec95dc90f247b3a26fcd2484bd5fbdc0a2
abdelmac <abdelkadermachta@gmail.com>
2024-11-08 05:08:16 +0100

    dz

--- sudo.py
+++ sudo.py
@@ -34,7 +34,7 @@ for i in range(3):
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
 reponse="true"
-r=1
+r=123456789101112131415161718192021222324252627282930
 for i in range(9):
     for j in range(9):
         if (L[i][j] in L[i][-j:]) or (L[i][j] in L[i][:j]):
